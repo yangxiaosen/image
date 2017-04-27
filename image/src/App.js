58aa12641a92b42654ac1b42b28513dc18002c8d
yangxiaosen <2660591904@qq.com>
2017-04-27 15:29:07 +0800

    demo

--- image/src/App.js
+++ image/src/App.js
@@ -11,12 +11,23 @@ class App extends Component {
           <h2>图像处理demo</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          通过这里的图片处理，你可以美化你的图片.
         </p>
         <div className="img-content">
            <div className="img-before"></div>
            <div className="img-after"></div>
-           <button></button>
+           <button className="btn-before">处理前</button>
+           <button className="btn-after">处理后</button>
+        </div>
+        <button className="btn-one">一键处理</button>
+        <div className="btn-choose">
+            <div>美白</div>
+            <div>滤镜</div>
+            <div>水印</div>
+            <div>镜像</div>
+            <div>裁剪</div>
+            <div>拼接</div>
+            <div>旋转</div>
         </div>
       </div>
     );
