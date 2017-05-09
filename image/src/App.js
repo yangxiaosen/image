6771a78a0559c2a841f8c5d67fddad45fc5aa5ed
yangxiaosen <2660591904@qq.com>
2017-05-09 15:42:07 +0800

    已经有例子可以运行

--- image/src/App.js
+++ image/src/App.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Btn from './Btn';
 import './App.css';
 
 class App extends Component {
+ /* getInitialState(){
+    return {
+      background:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493287035729&di=0f17e0ebf81026d64ea8294408dc0ea4&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd53f8794a4c27d1ee59e974819d5ad6edcc43885.jpg"
+    };
+  }*/
+  constructor(props) {
+    super(props);
+    this.state = { background: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493287035729&di=0f17e0ebf81026d64ea8294408dc0ea4&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd53f8794a4c27d1ee59e974819d5ad6edcc43885.jpg",
+                   after: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493287035729&di=0f17e0ebf81026d64ea8294408dc0ea4&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd53f8794a4c27d1ee59e974819d5ad6edcc43885.jpg"};
+  }
+
+  handleClick(){
+    //
+    var self=this;
+    var imageinput=this.refs.imageinput;
+    imageinput.click();
+  }
+
+  handleChange(){
+    //this.setState({background:"http://img2.imgtn.bdimg.com/it/u=48219243,819110730&fm=214&gp=0.jpg"});
+    var self=this;
+    var imageinput=this.refs.imageinput;
+    var image=imageinput.files[0];
+    var oFReader = new FileReader();
+    oFReader.readAsDataURL(image);
+    oFReader.onload=function(e){
+      self.setState({background:e.target.result,
+        after:e.target.result});
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,21 +46,18 @@ class App extends Component {
           通过这里的图片处理，你可以美化你的图片.
         </p>
         <div className="img-content">
-           <div className="img-before"></div>
-           <div className="img-after"></div>
-           <button className="btn-before">处理前</button>
-           <button className="btn-after">处理后</button>
+           <div className="img-before">
+             <input type="file" ref="imageinput" id="imageinput" onChange={this.handleChange.bind(this)}/>
+             <img src={this.state.background} className="image-content" />
+           </div>
+           <div className="img-after"><img src={this.state.after} className="image-content" id="imagehandle" /></div>
+           <button className="btn-before" onClick={this.handleClick.bind(this)}>上传图片</button>
+           <a href={this.state.after} download="已处理">
+           <button className="btn-after">保存图片</button>
+           </a>
         </div>
         <button className="btn-one">一键处理</button>
-        <div className="btn-choose">
-            <div>美白</div>
-            <div>滤镜</div>
-            <div>水印</div>
-            <div>镜像</div>
-            <div>裁剪</div>
-            <div>拼接</div>
-            <div>旋转</div>
-        </div>
+        <Btn/>
       </div>
     );
   }
---
+++ image/src/Btn.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './Btn.css';
+import fx from 'glfx';
+class Btn extends Component {
+	handleClick(){
+		    try {
+		        var canvas = fx.canvas();
+		    } catch (e) {
+		        alert(e);
+		        return;
+		    }
+		    var image = document.getElementById('imagehandle');
+            var texture = canvas.texture(image);
+            canvas.draw(texture).ink(0.50).update();
+            console.log(canvas);
+            /*image.parentNode.insertBefore(canvas, image);
+            image.parentNode.removeChild(image);*/
+            image.src=canvas.toDataURL();
+	}
+
+	render(){
+		return(
+			<div className="btn-choose">
+			<div onClick={this.handleClick.bind(this)}>油画</div>
+            <div>基础编辑</div>
+            <div>滤镜</div>
+            <div>饰品</div>
+            <div>边框</div>
+            <div>场景</div>
+            <div>拼图</div>
+            </div>
+			);
+	}
+}
+
+export default Btn;
