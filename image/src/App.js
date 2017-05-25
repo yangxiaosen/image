b93c8b57cd46758d9899d057baf46d0e09db56b4
yangxiaosen <2660591904@qq.com>
2017-05-25 15:47:58 +0800

    90%

--- image/src/App.js
+++ image/src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { background: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493287035729&di=0f17e0ebf81026d64ea8294408dc0ea4&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd53f8794a4c27d1ee59e974819d5ad6edcc43885.jpg",
-                   after: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493287035729&di=0f17e0ebf81026d64ea8294408dc0ea4&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd53f8794a4c27d1ee59e974819d5ad6edcc43885.jpg"};
+                   after: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493287035729&di=0f17e0ebf81026d64ea8294408dc0ea4&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd53f8794a4c27d1ee59e974819d5ad6edcc43885.jpg",
+                   sign:"hhh"};
   }
 
   handleClick(){
@@ -25,6 +26,7 @@ class App extends Component {
   handleChange(){
     //this.setState({background:"http://img2.imgtn.bdimg.com/it/u=48219243,819110730&fm=214&gp=0.jpg"});
     var self=this;
+    this.submitimg();
     var imageinput=this.refs.imageinput;
     var image=imageinput.files[0];
     var oFReader = new FileReader();
@@ -33,6 +35,80 @@ class App extends Component {
       self.setState({background:e.target.result,
         after:e.target.result});
     }
+  }
+
+  drop(e){
+    e.preventDefault();
+    var data=e.dataTransfer.getData("Text");
+    var target=e.dataTransfer.getData("Target");
+    console.log(target);
+    console.log(data);
+    //获取图片位置
+    var imaget=e.target.getBoundingClientRect().top;
+    var imagel=e.target.getBoundingClientRect().left;
+    console.log(e.target.getBoundingClientRect().top);
+    console.log(e.target.getBoundingClientRect().left);
+    //获取鼠标位置
+    var clientt=e.clientY-20;
+    var clientl=e.clientX-50;
+    //计算相对位置
+    console.log(e.clientX-imagel);
+    console.log(e.clientY-imaget);
+
+    var dataid;
+    if(/copy/g.test(data)){
+      dataid=data;
+    }else{
+      dataid=data+"copy";
+    }
+    var tt=document.getElementById(dataid);
+    console.log(tt);
+    tt.style.top=clientt+"px";
+    tt.style.left=clientl+"px";
+  }
+
+  allowDrop(e){
+    e.preventDefault();
+  }
+
+  //提交图片和标识符
+  submitimg(){
+    //获取图片文件
+    var imageinput=this.refs.imageinput;
+    var image=imageinput.files[0];
+    var formData = new FormData();
+    formData.append('image',image);
+    //获取日期时间作为标识符
+    var sign=new Date();
+    formData.append('sign',sign.getTime());
+    console.log(sign.getTime());
+    //提交数据
+    var url;
+    var obj=new XMLHttpRequest();  // XMLHttpRequest对象用于在后台与服务器交换数据          
+    obj.open('POST',url,true);
+    obj.onreadystatechange=function(){
+        if (obj.readyState == 4 && obj.status == 200) { // readyState==4说明请求已完成
+            //fn.call(this, obj.responseText)从服务器获得数据
+            this.setState({sign:sign.getTime()+''});
+        }
+    };
+    obj.send(formData);
+  }
+  //提交操作
+  submitshow(action,x,y){
+    var obj=new XMLHttpRequest();  // XMLHttpRequest对象用于在后台与服务器交换数据          
+      var url;
+      x=x/480;
+      y=y/350;
+      url=url+'?'+action+'="'+x+':'+y+'"';
+      obj.open('GET',url,true);
+      obj.onreadystatechange=function(){
+          if (obj.readyState == 4 && obj.status == 200) { // readyState==4说明请求已完成
+              //fn.call(this, obj.responseText)从服务器获得数据
+              //this.setState({sign:sign.getTime()+''});
+          }
+      };
+      obj.send();
   }
 
   render() {
@@ -50,14 +126,14 @@ class App extends Component {
              <input type="file" ref="imageinput" id="imageinput" onChange={this.handleChange.bind(this)}/>
              <img src={this.state.background} className="image-content" />
            </div>
-           <div className="img-after"><img src={this.state.after} className="image-content" id="imagehandle" /></div>
+           <div className="img-after" id="imgafter" onDrop={this.drop} onDragOver={this.allowDrop}><img src={this.state.after} className="image-content" id="imagehandle" /></div>
            <button className="btn-before" onClick={this.handleClick.bind(this)}>上传图片</button>
            <a href={this.state.after} download="已处理">
            <button className="btn-after">保存图片</button>
            </a>
         </div>
         <button className="btn-one">一键处理</button>
-        <Btn/>
+        <Btn sign={this.state.sign}/>
       </div>
     );
   }
--- image/src/Btn.js
+++ image/src/Btn.js
@@ -19,16 +19,94 @@ class Btn extends Component {
             image.src=canvas.toDataURL();
 	}
 
+	btnClick1(e){
+		//
+		var listdom=e.target;
+		var targetdom=listdom.getAttribute("data-target");
+		//listdom.style.visibility='hidden';
+		var status=document.getElementById(targetdom);
+		//.style.visibility;
+		if(status.style.visibility=="hidden"){
+			status.style.visibility="visible";
+		}else{
+			status.style.visibility="hidden";
+		}
+	}
+
+	drag(e){
+		e.dataTransfer.setData("Text",e.target.id);
+		e.dataTransfer.setData("Target",e.target.getAttribute("data-target"));
+		console.log(e.target);
+	}
+
+	
+	//提交操作,不需要坐标参数
+	submitshow1(action){
+		var obj=new XMLHttpRequest();  // XMLHttpRequest对象用于在后台与服务器交换数据          
+	    var url;
+	    url=url+'?'+action+'='+action;
+	    obj.open('GET',url,true);
+	    obj.onreadystatechange=function(){
+	        if (obj.readyState == 4 && obj.status == 200) { // readyState==4说明请求已完成
+	            //fn.call(this, obj.responseText)从服务器获得数据
+	            //this.setState({sign:sign.getTime()+''});
+	        }
+	    };
+	    obj.send();
+	}
+
+	//刷新图标位置
+	freshicon(){
+		//获取图标dom
+		document.getElementByClassName("btn-listcopy");
+	}
+
 	render(){
 		return(
+			<div>
 			<div className="btn-choose">
-			<div onClick={this.handleClick.bind(this)}>油画</div>
-            <div>基础编辑</div>
-            <div>滤镜</div>
-            <div>饰品</div>
-            <div>边框</div>
-            <div>场景</div>
-            <div>拼图</div>
+				<div onClick={this.handleClick.bind(this)}>油画</div>
+	            <div data-target="btn-list1" onClick={this.btnClick1.bind(this)}>基础编辑</div>
+	            <div data-target="btn-list2" onClick={this.btnClick1.bind(this)}>饰品</div>
+	            <div data-target="btn-list3" onClick={this.btnClick1.bind(this)}>边框</div>
+	            <div data-target="btn-list4" onClick={this.btnClick1.bind(this)}>场景</div>
+            </div>
+            <div id="btn-list1" className="btn-list" ref="basicevent">
+            	<div className="glfxshow">inh</div>
+	            <div className="glfxshow">blur</div>
+	            <div className="glfxshow">lens blur</div>
+	            <div className="glfxshow">vignetle</div>
+	            <div className="glfxshow">sepig</div>
+	            <div className="glfxshow">something</div>
+            </div>
+            <div id="btn-list2" className="btn-list">
+            	<div className="watermark1" id="watermark1" data-target="shipin1" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark2" id="watermark2" data-target="shipin2" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark3" id="watermark3" data-target="shipin3" draggable="true" onDragStart={this.drag}></div>
+	            <div>文字</div>
+	            <div className="watermark4" id="watermark4" data-target="shipin4" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark5" id="watermark5" data-target="shipin5" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark6" id="watermark6" data-target="shipin6" draggable="true" onDragStart={this.drag}></div>
+            </div>
+            <div className="btn-listcopy">
+            	<div className="watermark1" id="watermark1copy" data-target="shipin1" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark2" id="watermark2copy" data-target="shipin2" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark3" id="watermark3copy" data-target="shipin3" draggable="true" onDragStart={this.drag}></div>
+	            <div>文字</div>
+	            <div className="watermark4" id="watermark4copy" data-target="shipin4" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark5" id="watermark5copy" data-target="shipin5" draggable="true" onDragStart={this.drag}></div>
+	            <div className="watermark6" id="watermark6copy" data-target="shipin6" draggable="true" onDragStart={this.drag}></div>
+            </div>
+            <div id="btn-list3" className="btn-list">
+            	<div className="border1">边框1</div>
+	            <div className="border2">边框2</div>
+	            <div className="border3">边框3</div>
+            </div>
+            <div id="btn-list4" className="btn-list">
+            	<div className="border4"></div>
+	            <div className="border5"></div>
+	            <div className="border6"></div>
+            </div>
             </div>
 			);
 	}
